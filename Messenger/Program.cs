1d650b88ca83cae67dcc3e8af57f18314dcd1d7d
jwy4877 <jwy4877@g.rit.edu>
2020-03-21 16:53:46 -0400

    + checks options for validity

--- Messenger/Program.cs
+++ Messenger/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Messenger
 {
+    internal static class Extension
+    {
+        static BigInteger modInverse( BigInteger a, BigInteger n )
+        {
+            BigInteger i = n, v = 0, d = 1;
+            while ( a > 0 )
+            {
+                BigInteger t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if ( v < 0 ) v = ( v + n ) % n;
+            return v;
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        private static void Usage()
+        {   
+            string usageText = "dotnet run <option> <other arguments>\n";
+            usageText += $"{"<option>:", 12} keyGen, sendKey, getKey, sendMsg, getMsg\n";
+            usageText += $"{"<other args>:", 12}\n";
+            Console.WriteLine( usageText );
+            Environment.Exit( 1 );
+        }
+        
+        private enum Options
         {
-            Console.WriteLine("Hello World!");
+            KeyGen,
+            SendKey,
+            GetKey,
+            SendMsg,
+            GetMsg,
+            Invalid
+        }
+
+        private static Options GetOptions( string arg )
+        {
+            var val = arg switch
+            {
+                "keyGen" => Options.KeyGen,
+                "sendKey" => Options.SendKey,
+                "getKey" => Options.GetKey,
+                "sendMsg" => Options.SendMsg,
+                "getMsg" => Options.GetMsg,
+                _ => Options.Invalid
+            };
+
+            return val;
+        }
+        
+        public static void Main( string[] args )
+        {
+            if ( args.Length != 2 ) Usage();
+            var option = GetOptions( args[0] );
+            
         }
     }
 }
